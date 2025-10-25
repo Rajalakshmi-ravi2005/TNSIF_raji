b1ff1d0d305bc25f548e6e7d1585e3820a304a56
Rajalakshmi-ravi2005 <rajalakshmiravi030505@gmail.com>
2025-10-25 15:06:52 +0530

    This is day 9

---
+++ Tnsf_java_raji/src/day9_junit_testing/MyFirstTest.java
@@ -0,0 +1,15 @@
+package day9_junit_testing;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+	
+	@Test //basic testing annotation
+	void display()
+	{
+		System.out.println("Hello World");
+	}
+	
+	
+
+}
--- Tnsf_java_raji/src/module-info.java
+++ Tnsf_java_raji/src/module-info.java
@@ -5,4 +5,7 @@
  * 
  */
 module Tnsf_java_raji {
+	requires junit;
+	requires org.junit.jupiter.api;
+	requires org.junit.jupiter.params;
 }
